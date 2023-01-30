8c563194226c58227d8f8d7e054c6041b51b8660
kerry91 <kerry91warnes@gmail.com>
2023-01-30 19:57:50 +0000

    update to hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,22 +3,44 @@ import "../css/Hero.css";
 const Hero = (props) => {
   return (
     <>
-      <container class="hero-container">
-        <div>
+      {/*
+      <section class="hero-container">
+        <article>
           <h2>Little Lemon</h2>
           <h3>Chicago</h3>
           <p class="description">{props.desc}</p>
           <p class="button">{props.button}</p>
-        </div>
-        <h2>{props.extraText}</h2>
+        </article>
+        
+        <article>
         <div>
           <img src={props.src} alt={props.alt} />
         </div>
-
         <div>
           <img src={props.src2} alt={props.alt2} />
         </div>
-      </container>
+        </article>
+      </section>
+  */}
+      <section class="hero-wrapper">
+        <div class="hero">
+          <div class="hero-card">
+            <h2>Little Lemon</h2>
+            <h3>Chicago</h3>
+            <p class="description">{props.desc}</p>
+            <p class="button">{props.button}</p>
+          </div>
+          <div class="hero-card">
+          <h2>{props.extraText}</h2>
+            <div>
+              <img src={props.src} alt={props.alt} />
+            </div>
+            <div>
+              <img src={props.src2} alt={props.alt2} />
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
