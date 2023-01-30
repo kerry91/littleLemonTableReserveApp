150bc3277f066f6772a96e323a7a509b97b690c5
kerry91 <kerry91warnes@gmail.com>
2023-01-30 13:15:20 +0000

    update to add more responsive grid to testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,33 +4,29 @@ import image2 from "../assets/lemon dessert.jpg";
 const Testimonials = () => {
   return (
     <>
-      <section class="testimonials-container">
-        <section class="testimonials-wrapper">
-          <div class="title">
-            <h3 id="testimonials-title">Testimonials</h3>
-          </div>
-          <div class="item1">
-            <p>Title</p>
-            <img src={image2} alt="greek salad" />
-            <p>Great</p>
-          </div>
-          <div class="item2">
-            <p>Title</p>
-            <img src={image2} alt="lemon dessert" />
-            <p>Great</p>
-          </div>
-          <div class="item3">
-            <p>Title</p>
-            <img src={image2} alt="lamb kebabs" />
-            <p>Great</p>
-          </div>
-          <div class="item4">
-            <p>Title</p>
-            <img src={image2} alt="lemon dessert" />
-            <p>Great</p>
-          </div>
-        </section>
-      </section>
+   <div class="testimonials">
+  <div class="testimonial-card">
+    <p>Title</p>
+    <img src={image2} alt="image1"/>
+    <p>Great</p>
+    </div>
+  <div class="testimonial-card">
+  <p>Title</p>
+    <img src={image2} alt="image1"/>
+    <p>Great</p>
+  </div>
+  <div class="testimonial-card">
+  <p>Title</p>
+    <img src={image2} alt="image1"/>
+    <p>Great</p>
+  </div>
+  <div class="testimonial-card">
+  <p>Title</p>
+    <img src={image2} alt="image1"/>
+    <p>Great</p>
+  </div>
+ 
+</div>
     </>
   );
 };
